0405e5fdf5545fdae5bb6f60ed42d1c4b8e76723
Your Name <you@example.com>
2026-03-21 22:39:54 -0300

    add

--- guia-estudante-unama/Páginas/Pag-Lab/main.js
+++ guia-estudante-unama/Páginas/Pag-Lab/main.js
@@ -1,6 +1,16 @@
-function reservar(lab) {
-    alert("Você reservou o " + lab);
-}z
-function toggleMenu(menu) {
-    menu.classList.toggle("active");
+function reservar(botao) {
+
+  // pega o card (caixa do laboratório)
+  const card = botao.closest(".card");
+
+  // pega o nome do labz
+  const lab = card.querySelector(".badge").innerText;
+
+  // muda o botão
+  botao.innerText = "Reservado";
+  botao.style.backgroundColor = "#999";
+  botao.disabled = true;
+
+  // opcional: mostrar alerta
+  alert(lab + " reservado com sucesso!");
 }
